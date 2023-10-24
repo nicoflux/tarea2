0bd941a88c355a4bf7d967a0274bcbe1e63b95b5
nicoflux <nicolas.bloch1234@gmail.com>
2023-10-23 22:03:15 -0300

    update cliente y ventas

---
+++ cliente y ventas/VM1/cliente.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"grpc-golang/pb"
+	"log"
+	"os"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	fmt.Println("Hello buyer ...")
+	// Check if a JSON file name was provided as a command-line argument
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: go run client.go <json_file_name>")
+		return
+	}
+
+	jsonFileName := os.Args[1]
+
+	// Read order data from the specified JSON file
+	data, err := os.ReadFile(jsonFileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var request pb.OrderServiceRequest
+	json.Unmarshal(data, &request)
+
+	conn, err := grpc.Dial("localhost:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	client := pb.NewOrderServiceClient(conn)
+	resp, err := client.Order(context.Background(), &request)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Receive response => %s ", resp.OrderResponse)
+}
