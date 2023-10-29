b24dd2541ca29df602da545a158d69ad05675428
nicoflux <nicolas.bloch1234@gmail.com>
2023-10-29 02:36:18 -0300

    update ventas.go

--- final/VM2/ventas.go
+++ final/VM2/ventas.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"bytes"
 	"context"
-	"encoding/gob"
 	"encoding/json"
 	"fmt"
 	"grpc-golang/pb"
@@ -20,10 +18,10 @@ import (
 
 var mongo_Client *mongo.Client
 
-var despachoQueue amqp.Queue
+/* var despachoQueue amqp.Queue
 var inventarioQueue amqp.Queue
 var notificacionQueue amqp.Queue
-var ch *amqp.Channel
+var ch *amqp.Channel */
 
 type server struct {
 	pb.OrderServiceServer
@@ -72,7 +70,7 @@ type Order struct {
 	} `json:"deliveries"`
 }
 
-func startRabbitMQ() {
+/* func startRabbitMQ() {
 	// Configura la conexión a RabbitMQ
 	conn, err := amqp.Dial("amqp://guest:guest@10.10.11.233:5672/")
 	if err != nil {
@@ -104,57 +102,58 @@ func startRabbitMQ() {
 
 	fmt.Println("Colas de RabbitMQ creadas con éxito")
 
-}
+} */
 
-func sendRabbitMQ(order Order) {
+/*
+	 func sendRabbitMQ(order Order) {
 
-	// Encode it to a byte array using gob
-	var buf bytes.Buffer
-	enc := gob.NewEncoder(&buf)
-	err := enc.Encode(order)
-	if err != nil {
-		panic(err)
+		// Encode it to a byte array using gob
+		var buf bytes.Buffer
+		enc := gob.NewEncoder(&buf)
+		err := enc.Encode(order)
+		if err != nil {
+			panic(err)
+		}
+
+		// Convert buffer to byte array
+		data := buf.Bytes()
+
+		fmt.Println("Publicando estructura de orden en RabbitMQ para despacho, inventario y notificación")
+
+		// Publica la estructura de orden en las colas de los servicios
+		err = ch.Publish("", despachoQueue.Name, false, false, amqp.Publishing{
+			ContentType: "application/json",
+			Body:        []byte(data),
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = ch.Publish("", inventarioQueue.Name, false, false, amqp.Publishing{
+			ContentType: "application/json",
+			Body:        []byte(data),
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = ch.Publish("", notificacionQueue.Name, false, false, amqp.Publishing{
+			ContentType: "application/json",
+			Body:        []byte(data),
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("Estructura de orden publicada en RabbitMQ para despacho, inventario y notificación")
 	}
-
-	// Convert buffer to byte array
-	data := buf.Bytes()
-
-	fmt.Println("Publicando estructura de orden en RabbitMQ para despacho, inventario y notificación")
-
-	// Publica la estructura de orden en las colas de los servicios
-	err = ch.Publish("", despachoQueue.Name, false, false, amqp.Publishing{
-		ContentType: "application/json",
-		Body:        []byte(data),
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = ch.Publish("", inventarioQueue.Name, false, false, amqp.Publishing{
-		ContentType: "application/json",
-		Body:        []byte(data),
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = ch.Publish("", notificacionQueue.Name, false, false, amqp.Publishing{
-		ContentType: "application/json",
-		Body:        []byte(data),
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Estructura de orden publicada en RabbitMQ para despacho, inventario y notificación")
-}
-
+*/
 func (s *server) Order(ctx context.Context, req *pb.OrderServiceRequest) (*pb.OrderServiceReply, error) {
 	receivedJSON, err := json.Marshal(req)
 	if err != nil {
 		return nil, err
 	}
-	log.Printf("Recibido : %s", string(receivedJSON))
+	log.Printf("Order Recibido")
 	var order Order
 	err = json.Unmarshal(receivedJSON, &order)
 	if err != nil {
@@ -166,6 +165,94 @@ func (s *server) Order(ctx context.Context, req *pb.OrderServiceRequest) (*pb.Or
 	return &pb.OrderServiceReply{
 		OrderResponse: fmt.Sprintf("You order id is : %s", orderId),
 	}, nil
+}
+
+func sendRabbitMQ(order Order) {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ch.Close()
+
+	// Declara tres colas para los servicios de despacho, inventario y notificación
+	despachoQueue, err := ch.QueueDeclare("despacho-cola", false, false, false, false, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	inventarioQueue, err := ch.QueueDeclare("inventario-cola", false, false, false, false, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	notificacionQueue, err := ch.QueueDeclare("notificacion-cola", false, false, false, false, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Define una estructura de orden de ejemplo
+	/* 	orderData := `{
+		"products": [
+			{
+				"title": "The Lord of the Rings",
+				"quantity": 2,
+				"price": 20
+			}
+			// Agrega el resto de los productos aquí...
+		],
+		"customer": {
+			"name": "John",
+			"lastname": "Doe",
+			"email": "hleytondiaz@gmail.com",
+			"location": {
+				"address1": "123 Main Street",
+				"address2": "Apt. 1",
+				"city": "Anytown",
+				"state": "CA",
+				"postalCode": "91234",
+				"country": "USA"
+			},
+			"phone": "555-555-5555"
+		}
+	}` */
+
+	// Publica la estructura de orden en las colas de los servicios
+	orderBytes, err := json.Marshal(order)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ch.Publish("", despachoQueue.Name, false, false, amqp.Publishing{
+		ContentType: "application/json",
+		Body:        orderBytes,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ch.Publish("", inventarioQueue.Name, false, false, amqp.Publishing{
+		ContentType: "application/json",
+		Body:        orderBytes,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ch.Publish("", notificacionQueue.Name, false, false, amqp.Publishing{
+		ContentType: "application/json",
+		Body:        orderBytes,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Estructura de orden publicada en RabbitMQ para despacho, inventario y notificación")
 }
 
 func connectToMongoDB() (*mongo.Client, error) {
