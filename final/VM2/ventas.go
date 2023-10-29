2431cf55fbefa8a35922b24c89cafa0fea91a816
nicoflux <nicolas.bloch1234@gmail.com>
2023-10-29 02:44:29 -0300

    update ventas.go

--- final/VM2/ventas.go
+++ final/VM2/ventas.go
@@ -18,11 +18,6 @@ import (
 
 var mongo_Client *mongo.Client
 
-var despachoQueue amqp.Queue
-var inventarioQueue amqp.Queue
-var notificacionQueue amqp.Queue
-var ch *amqp.Channel
-
 type server struct {
 	pb.OrderServiceServer
 }
@@ -70,77 +65,6 @@ type Order struct {
 	} `json:"deliveries"`
 }
 
-func startRabbitMQ() {
-	// Configura la conexión a RabbitMQ
-	conn, err := amqp.Dial("amqp://guest:guest@10.10.11.233:5672/")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer conn.Close()
-
-	ch, err := conn.Channel()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer ch.Close()
-
-	// Declara tres colas para los servicios de despacho, inventario y notificación
-	despachoQueue, err = ch.QueueDeclare("despacho-cola", false, false, false, false, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	inventarioQueue, err = ch.QueueDeclare("inventario-cola", false, false, false, false, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	notificacionQueue, err = ch.QueueDeclare("notificacion-cola", false, false, false, false, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Colas de RabbitMQ creadas con éxito")
-
-}
-
-func sendRabbitMQ(order Order) {
-
-	// Encode it to a byte array using gob
-	orderBytes, err := json.Marshal(order)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Publicando estructura de orden en RabbitMQ para despacho, inventario y notificación")
-
-	// Publica la estructura de orden en las colas de los servicios
-	err = ch.Publish("", despachoQueue.Name, false, false, amqp.Publishing{
-		ContentType: "application/json",
-		Body:        orderBytes,
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = ch.Publish("", inventarioQueue.Name, false, false, amqp.Publishing{
-		ContentType: "application/json",
-		Body:        orderBytes,
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = ch.Publish("", notificacionQueue.Name, false, false, amqp.Publishing{
-		ContentType: "application/json",
-		Body:        orderBytes,
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Estructura de orden publicada en RabbitMQ para despacho, inventario y notificación")
-}
-
 func (s *server) Order(ctx context.Context, req *pb.OrderServiceRequest) (*pb.OrderServiceReply, error) {
 	receivedJSON, err := json.Marshal(req)
 	if err != nil {
@@ -160,7 +84,7 @@ func (s *server) Order(ctx context.Context, req *pb.OrderServiceRequest) (*pb.Or
 	}, nil
 }
 
-/* func sendRabbitMQ(order Order) {
+func sendRabbitMQ(order Order) {
 	conn, err := amqp.Dial("amqp://guest:guest@10.10.11.233:5672/")
 	if err != nil {
 		log.Fatal(err)
@@ -189,32 +113,6 @@ func (s *server) Order(ctx context.Context, req *pb.OrderServiceRequest) (*pb.Or
 		log.Fatal(err)
 	}
 
-	// Define una estructura de orden de ejemplo
-	 	orderData := `{
-		"products": [
-			{
-				"title": "The Lord of the Rings",
-				"quantity": 2,
-				"price": 20
-			}
-			// Agrega el resto de los productos aquí...
-		],
-		"customer": {
-			"name": "John",
-			"lastname": "Doe",
-			"email": "hleytondiaz@gmail.com",
-			"location": {
-				"address1": "123 Main Street",
-				"address2": "Apt. 1",
-				"city": "Anytown",
-				"state": "CA",
-				"postalCode": "91234",
-				"country": "USA"
-			},
-			"phone": "555-555-5555"
-		}
-	}`
-
 	// Publica la estructura de orden en las colas de los servicios
 	orderBytes, err := json.Marshal(order)
 	if err != nil {
@@ -246,7 +144,7 @@ func (s *server) Order(ctx context.Context, req *pb.OrderServiceRequest) (*pb.Or
 	}
 
 	fmt.Println("Estructura de orden publicada en RabbitMQ para despacho, inventario y notificación")
-} */
+}
 
 func connectToMongoDB() (*mongo.Client, error) {
 	//URI := os.Getenv("CONNECTION_STRING")
@@ -301,7 +199,6 @@ func main() {
 		fmt.Println("Error al conectar a MongoDB:", err)
 	}
 	defer closeMongoDBConnection(mongo_Client)
-	startRabbitMQ()
 	listener, err := net.Listen("tcp", ":8080")
 	fmt.Println("Server is running on port 8080")
 	if err != nil {
